30f1a5ab71aecd1d9f7e6d6c06b384ccdef86d70
debug-log <achileus2003@naver.com>
2018-08-19 16:45:38 +0900

    First Commit

---
+++ Assets/Editor/EditorSceneSetup.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public static class EditorSceneSetup {
+
+    private static Dictionary<string, int> priorityTable = new Dictionary<string, int> ();
+
+    [MenuItem ("Load Scene/Generate Scene Menus")]
+    static void Generate()
+    {
+        TextAsset templateMenuItem = Resources.Load ("TemplateMenuItem") as TextAsset;
+        StringBuilder stringBuilder = new StringBuilder ();
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        
+        for (int i = 0; i < sceneCount; ++i)
+        {
+            int level = GetCommonCategoryLevel ();
+            string path = SceneUtility.GetScenePathByBuildIndex (i);
+            string name = Path.GetFileNameWithoutExtension (path);
+            string category = path.Split ('/').GetValue (level) as string;
+
+            if (priorityTable.ContainsKey (category))
+            {
+                priorityTable[category]++;
+            }
+            else
+            {
+                priorityTable.Add (category, priorityTable.Keys.Count * sceneCount * 10 + 1);
+            }
+
+            int priority = priorityTable[category];
+            string menuItemText = templateMenuItem.text;
+            menuItemText = menuItemText.Replace ("#ScenePath", path);
+            menuItemText = menuItemText.Replace ("#SceneName", name);
+            menuItemText = menuItemText.Replace ("#Priority", priority.ToString());
+            stringBuilder.AppendLine (menuItemText);
+        }
+
+
+        TextAsset templateEditorSceneSetup = Resources.Load ("TemplateEditorSceneSetup") as TextAsset;
+        string output = templateEditorSceneSetup.text.Replace ("#MenuItemList", stringBuilder.ToString ());
+
+
+        string scriptFile = Application.dataPath + "/Editor/GeneratedSceneSetupMenuItem.cs";
+        System.IO.File.Delete (scriptFile);
+        System.IO.File.WriteAllText (scriptFile, output, System.Text.Encoding.UTF8);
+        AssetDatabase.ImportAsset ("Assets/Editor/GeneratedSceneSetupMenuItem.cs");
+
+        Debug.Log ("Create file [Assets/Editor/EditorSceneSetup.cs]");
+    }
+
+    static int GetCommonCategoryLevel ()
+    {
+        int level = 0;
+        string currentFolderName = "";
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        List<string[]> pathList = new List<string[]> ();
+
+        //Init
+        for (int i = 0; i < sceneCount; ++i)
+        {
+            string path = SceneUtility.GetScenePathByBuildIndex (i);
+            pathList.Add (path.Split ('/'));
+        }
+
+        //Find the lowest-level folder name that is not common
+        while(true)
+        {
+            currentFolderName = pathList[0].GetValue (level) as string;
+
+            foreach (var path in pathList)
+            {
+                if (path.GetValue (level) == null)
+                {
+                    return level;
+                }
+
+                string nextFolderName = path.GetValue (level) as string;
+                if(!currentFolderName.Equals(nextFolderName))
+                {
+                    return level;
+                }
+            }
+
+            ++level;
+        }
+    }
+}
---
+++ Assets/Editor/GeneratedSceneSetupMenuItem.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace LoadSceneWithMenuItem
+{
+    public static class MenuItemScene
+    {
+        private static bool toggleAdditive;
+
+        [MenuItem ("Load Scene/Additive Mode %#t")]
+        static void ToggleAdditiveMode ()
+        {
+            toggleAdditive = !toggleAdditive;
+            Menu.SetChecked ("Load Scene/Additive Mode", toggleAdditive);
+        }
+
+        [MenuItem ("Load Scene/MasterScene", false, 1)]
+        static void LoadMasterScene ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Common/MasterScene.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Common/MasterScene.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/BackDrop", false, 71)]
+        static void LoadBackDrop ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/BackDrop.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/BackDrop.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/Forest", false, 72)]
+        static void LoadForest ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/Forest.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/Forest.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/TreeAndBox", false, 73)]
+        static void LoadTreeAndBox ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/TreeAndBox.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/Environment/TreeAndBox.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/PlayZone", false, 141)]
+        static void LoadPlayZone ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/GamePlay/PlayZone.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/GamePlay/PlayZone.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/UIPopup", false, 211)]
+        static void LoadUIPopup ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/UI/UIPopup.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/UI/UIPopup.unity");
+            }
+        }
+
+        [MenuItem ("Load Scene/UIScene", false, 212)]
+        static void LoadUIScene ()
+        {
+            if (toggleAdditive)
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/UI/UIScene.unity", OpenSceneMode.Additive);
+            }
+            else
+            {
+                EditorSceneManager.OpenScene ("Assets/Scenes/UI/UIScene.unity");
+            }
+        }
+
+
+    }
+}
